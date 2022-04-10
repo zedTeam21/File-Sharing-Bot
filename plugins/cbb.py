f0b33cab6bde7940c8e53a37ce78d178999d1dce
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2022-04-10 12:01:15 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Created by : @techwrd x @server_z\n○ Support Group : <a href='https://t.me/+5q6MHdAAC3k2NDRl'>Join Now</a></b>",
+            text = f"<b>○ Creator : @xcybersecurity_bot\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Created by : @techwrd x @server_z\n○ Support Group : <a href='https://t.me/+5q6MHdAAC3k2NDRl'>Join Now</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
